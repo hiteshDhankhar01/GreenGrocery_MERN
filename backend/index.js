53f228b35c1afd033807d5e676537d382d95ea9a
Hitesh <hiteshkumarasdfghjkl@gmail.com>
2024-02-10 16:39:19 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,6 @@ const app = express();
 
 const connectDB = async () => {
     try { 
-        // Use the MONGO_URI from your environment variables (from your .env file)
         const mongoURI = process.env.MONGO_URL;
 
         // Connect to the MongoDB database
@@ -44,19 +43,9 @@ app.use('/api/v1/user', userRouter);
 app.use('/api/v1/product', productRouter);
 app.use('/api/v1/payment', payment)
 
-// app.put('/api/v1/user/update/:id', async (req, res) => {
-//     let result = await User.updateOne(
-//         { _id: req.params.id },
-//         { $set: req.body }
-//     )
-//     res.send(result)
-// })
-
-// ... other route configurations and server setup
-
-
-
 app.listen(port, () => {
     connectDB();
     console.log(`Server is running at http://127.0.0.1:${port}`);
 });
+
+
