a0cca91892ed72a5cdd4c75686dfa242dde8d421
Hitesh <hiteshkumarasdfghjkl@gmail.com>
2024-02-10 17:46:32 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -46,6 +46,12 @@ app.use('/', (req, res) => {
     res.send("serveris running")
 })
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.listen(port, () => {
     connectDB();
     console.log(`Server is running at http://127.0.0.1:${port}`);
