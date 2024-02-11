7ed13b7cb6de30f8b69e2b31cebe31a8c757990e
Hitesh <hiteshkumarasdfghjkl@gmail.com>
2024-02-11 20:28:16 +0530

    updated

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "https://vercel.com/hitesh07hiteshs-projects/green-grocery-mern/8Di3XgDSS2WSw8qgDoe54WBqNoBo/api/v1"
+export const BASE_URL = "https://vercel.com/hitesh07hiteshs-projects/green-grocery-mern/api/v1"
