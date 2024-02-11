bfe9fdd5e81798b1694d9e48c35c1c4d6502938c
Hitesh <hiteshkumarasdfghjkl@gmail.com>
2024-02-11 11:48:06 +0530

    hi

--- backend/index.js
+++ backend/index.js
@@ -34,6 +34,31 @@ const connectDB = async () => {
 // Middleware for CORS - should be placed before route handlers
 app.use(cors());
 
+
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+        return
+    }
+    return await fn(req, res)
+}
+
+const handler = (req, res) => {
+    const d = new Date()
+    res.end(d.toString())
+}
+
+app.use(allowCors(handler))
+
 // Middleware for parsing JSON
 app.use(express.json());
 
@@ -45,6 +70,9 @@ app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/product', productRouter);
 app.use('/api/v1/payment', payment);
+
+
+
 
 // Default route
 app.use('/', (req, res) => {
