cd297a7e80f3e4786db86e3bd51aa190762a4d73
Hitesh <hiteshkumarasdfghjkl@gmail.com>
2024-02-12 09:15:08 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const connectDB = async () => {
     try {
         const mongoURI = process.env.MONGO_URL;
 
-        // Connect to the MongoDB database
+
         await mongoose.connect(mongoURI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
@@ -27,43 +27,29 @@ const connectDB = async () => {
         console.log(`MongoDB connected successfully`);
     } catch (error) {
         console.error("MongoDB connection error:", error);
-        process.exit(1); // Exit the process with an error code
+        process.exit(1);
     }
 };
 
-// Middleware for CORS - should be placed before route handlers
-app.use(cors(
-    {
-        origin:["https://green-grocery-mern-2un1.vercel.app"],
-        methods:["POST","GET","DELETE","PUT"],
-        credentials:true
-    }
-));
 
-// Middleware for parsing JSON
+app.use(cors());
+
+
 app.use(express.json());
 
-// Middleware for parsing cookies
+
 app.use(cookieParser());
 
-// Routes
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/product', productRouter);
 app.use('/api/v1/payment', payment);
 
-// Default route
+
 app.use('/', (req, res) => {
     res.send("server is running");
 });
-
-// Error handling middleware, if needed
-// app.use(function(err, req, res, next) {
-//     console.error(err.stack);
-//     res.status(500).send('Something broke!');
-// });
-
-// CORS headers middleware - this might not be needed if already set up in the CORS package options
 
 app.listen(port, () => {
     connectDB();
